fea3a3a2dbd37361a2fab613878e153aa8927872
Harsh2116 <harshsolanki1946@gmail.com>
2025-06-13 18:39:17 +0530

    Update script.js

--- CrickTrades/script.js
+++ CrickTrades/script.js
@@ -1,7 +1,5 @@
 let stocks = []; // Initialize empty, will be populated from backend or WebSocket
 
-const BASE_URL = window.location.origin;
-
 // Function to fetch latest stock data from backend API
 async function fetchLatestStocks() {
     try {
