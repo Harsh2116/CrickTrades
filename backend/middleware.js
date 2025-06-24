eb46894d5e162c576a5fa494f56685c990f20c8c
Harsh2116 <harshsolanki1946@gmail.com>
2025-06-24 20:37:37 +0530

    Update middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -14,4 +14,16 @@ function authenticateAdminToken(req, res, next) {
     });
 }
 
-module.exports = { authenticateAdminToken };
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Access token missing' });
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { authenticateAdminToken, authenticateToken };
